42312f1b085552a20334408184c8afbd77ee58e0
Keali'i Transfield <liitransfield@gmail.com>
2021-10-19 21:11:36 -0600

    Made it so the "Game Actions" are able to pass arguments through them

--- Tintris_Game/Assets/0. TOOLS/z. Setup/Action Handler Setup/GameAction.cs
+++ Tintris_Game/Assets/0. TOOLS/z. Setup/Action Handler Setup/GameAction.cs
@@ -4,10 +4,32 @@ using UnityEngine.Events;
 [CreateAssetMenu]
 public class GameAction : ScriptableObject
 {
-    public UnityAction action;
+    public UnityAction<object> raise;
+    public UnityAction<Coroutine> raiseCoroutine;
+    public UnityAction raiseNoArgs;
     
-    public void Raise()
+    public void RaiseAction()
     {
-        action?.Invoke();
+        raiseNoArgs?.Invoke();
+    }
+    
+    public void RaiseAction(Object obj)
+    {
+        raise?.Invoke(obj);
+    }
+    
+    public void RaiseAction(int obj)
+    {
+        raise?.Invoke(obj);
+    }
+    
+    public void RaiseAction(Transform obj)
+    {
+        raise?.Invoke(obj);
+    }
+    
+    public void RaiseAction(Coroutine obj)
+    {
+        raise?.Invoke(obj);
     }
 }
--- Tintris_Game/Assets/0. TOOLS/z. Setup/Action Handler Setup/GameActionHandler.cs
+++ Tintris_Game/Assets/0. TOOLS/z. Setup/Action Handler Setup/GameActionHandler.cs
@@ -1,18 +1,36 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameActionHandler : MonoBehaviour
 {
-    public GameAction gameActionObj;
-    public UnityEvent onRaiseEvent;
+    public GameAction action;
+    public UnityEvent startEvent, respondEvent, respondLateEvent;
+    public float holdTime = 0.1f;
+
+    private WaitForSeconds waitObj;
     
     void Start()
     {
-        gameActionObj.action += ActionHandler;
+        startEvent.Invoke();
     }
 
-    private void ActionHandler()
+    private void OnEnable()
     {
-        onRaiseEvent.Invoke();
+        waitObj = new WaitForSeconds(holdTime);
+        action.raiseNoArgs += Respond;
+    }
+
+    private void Respond()
+    {
+        respondEvent.Invoke();
+        StartCoroutine(LateRespond());
+    }
+    
+    private IEnumerator LateRespond()
+    {
+        yield return waitObj;
+        respondLateEvent.Invoke();
     }
 }
