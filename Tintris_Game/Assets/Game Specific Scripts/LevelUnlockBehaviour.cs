563f83536ce58c0b4e3e39f654a2ae529fe4518c
Bravo64 <77293018+Bravo64@users.noreply.github.com>
2022-02-09 20:48:21 -0700

    Updated Level Unlock Int Numbers

--- Tintris_Game/Assets/Game Specific Scripts/LevelUnlockBehaviour.cs
+++ Tintris_Game/Assets/Game Specific Scripts/LevelUnlockBehaviour.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class LevelUnlockBehaviour : MonoBehaviour
 {
     public IntData highestLevelUnlocked;
+    public IntData currentLevel;
     public int sceneToNextLevelIndexShift = -1;
+    public int nextLevelNumber = 999;
     
     public void UnlockNextLevel()
     {
-        if (SceneManager.GetActiveScene().buildIndex + sceneToNextLevelIndexShift > highestLevelUnlocked.value)
+        if (nextLevelNumber > highestLevelUnlocked.value)
         {
-            highestLevelUnlocked.value = SceneManager.GetActiveScene().buildIndex + sceneToNextLevelIndexShift;
+            highestLevelUnlocked.value = nextLevelNumber;
         }
     }
 }
